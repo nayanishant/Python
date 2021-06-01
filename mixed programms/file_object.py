cdaee63d2c3030fd9fee99210e211da3c5ea21a8
nyanishant <nayanishant.info@gmail.com>
2021-06-01 23:10:06 +0530

    updated

--- mixed programms/file_object.py
+++ mixed programms/file_object.py
@@ -1,14 +1,19 @@
 with open ('D:\Knowledge\Programming Language\Exercise\Tw_text.txt', 'r+') as f:
+    # Read lines of file through iteration
     def word():
         for line in f:
             print(line, end = "")
 
     # word()
 
+    # Read lines of file through read method
     def read_file():
         read = f.read()
         print(read)
 
     # read_file()
+
+    
+        
 
     
