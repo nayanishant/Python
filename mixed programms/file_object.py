6f3378551153796028f4f623994e259dc8792c9c
nyanishant <nayanishant.info@gmail.com>
2021-06-01 22:07:12 +0530

    trying iteration and functions

--- mixed programms/file_object.py
+++ mixed programms/file_object.py
@@ -1,3 +1,14 @@
-with open ('D:\Knowledge\Programming Language\Exercise\Tw_text.txt', 'r') as f:
-    text = f.read()
-    print(text)
+with open ('D:\Knowledge\Programming Language\Exercise\Tw_text.txt', 'r+') as f:
+    def word():
+        for line in f:
+            print(line, end = "")
+
+    # word()
+
+    def read_file():
+        read = f.read()
+        print(read)
+
+    # read_file()
+
+    
